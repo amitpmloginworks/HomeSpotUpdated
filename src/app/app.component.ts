a2949bfc4dea410e844815b335d347432b25f00e
amitpm <amitpmloginworks@gmail.com>
2018-06-22 18:30:03 +0530

    new Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
+import { Nav, Platform, MenuController, ModalController, ActionSheetController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { CodecPage } from '../pages/codec/codec';
+import{MenuPage} from '../pages/menu/menu';
+import{ DeviceListPage } from '../pages/device-list/device-list';
+import{ CustomizationPage } from '../pages/customization/customization';
+import { ConnectionPage} from '../pages/connection/connection';
+import { TimeoutsPage} from '../pages/timeouts/timeouts';
+import { AdvancedPage} from '../pages/advanced/advanced';
+import { FeatureRequestPage} from '../pages/feature-request/feature-request';
+import { LrReceiverPage} from '../pages/lr-receiver/lr-receiver';
+import {ContactUsPage } from '../pages/contact-us/contact-us';
+import {AudioEffectPage } from '../pages/audio-effect/audio-effect';
+import {ProductRegistrationPage } from '../pages/product-registration/product-registration';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+
+
 
 @Component({
   templateUrl: 'app.html'
@@ -16,7 +31,7 @@ export class MyApp {
 
   pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(public platform: Platform, public modalCtrl:ModalController,public actionSheetCtrl: ActionSheetController, private iab: InAppBrowser, public statusBar: StatusBar, public menuCtrl: MenuController,  public splashScreen: SplashScreen) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
@@ -24,7 +39,8 @@ export class MyApp {
       { title: 'Home', component: HomePage },
       { title: 'List', component: ListPage }
     ];
-
+    // this.menuCtrl.enable(true, 'authenticated');
+    // this.menuCtrl.enable(false, 'unauthenticated');
   }
 
   initializeApp() {
@@ -40,5 +56,65 @@ export class MyApp {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
+  }
+
+  moveToHome(){
+    this.nav.setRoot(HomePage);
+  }
+  moveToProductRegistration(){
+    this.nav.setRoot(ProductRegistrationPage);
+  }
+  moveToFeatureRequest(){
+    this.nav.setRoot(FeatureRequestPage);
+  }
+  moveToContactUs(){
+    this.nav.setRoot(ContactUsPage);
+  }
+  moveToCustomization(){
+    this.nav.setRoot(CustomizationPage);
+
+  }
+  moveToAudioEffect(){
+   this.nav.setRoot(AudioEffectPage);
+  }
+  moveToDeviceList(){
+  // this.nav.setRoot(DeviceListPage);
+  let profileModal = this.modalCtrl.create(DeviceListPage);
+  profileModal.present();
+   console.log("device");
+
+  }
+
+  openInBrowser(){
+    const browser = this.iab.create('http://www.homespotdigital.com/');
+  }
+  unlink(){
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Unlink This Receiver. Are You Sure?',
+      buttons: [
+        {
+          text: 'Unlink',
+         
+          handler: () => {
+            console.log('Unlink clicked');
+          }
+        },
+       
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+ 
+    actionSheet.present();
+  }
+  goToLRReceiver(){
+    this.menuCtrl.close();
+    this.nav.setRoot(LrReceiverPage);
+    console.log('LrReceiverPage');
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,41 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { CodecPage } from '../pages/codec/codec';
+import{ MenuPage } from '../pages/menu/menu';
+import{ DeviceListPage } from '../pages/device-list/device-list';
+import{ CustomizationPage } from '../pages/customization/customization';
+import { ConnectionPage} from '../pages/connection/connection';
+import { TimeoutsPage} from '../pages/timeouts/timeouts';
+import { AdvancedPage} from '../pages/advanced/advanced';
+import { FeatureRequestPage} from '../pages/feature-request/feature-request';
+import { LrReceiverPage} from '../pages/lr-receiver/lr-receiver';
+import {ContactUsPage } from '../pages/contact-us/contact-us';
+import {AudioEffectPage } from '../pages/audio-effect/audio-effect';
+import {ProductRegistrationPage } from '../pages/product-registration/product-registration';
+
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { NativePageTransitions, NativeTransitionOptions } from '@ionic-native/native-page-transitions';
+import { DatePicker } from '@ionic-native/date-picker';
+
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    CodecPage,
+    MenuPage,
+    DeviceListPage,
+    CustomizationPage,
+    ConnectionPage,
+    TimeoutsPage,
+    AdvancedPage,
+    FeatureRequestPage,
+    LrReceiverPage,
+    ContactUsPage,
+    AudioEffectPage,
+    ProductRegistrationPage
   ],
   imports: [
     BrowserModule,
@@ -23,12 +52,30 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    CodecPage,
+    MenuPage,
+    DeviceListPage,
+    CustomizationPage,
+    ConnectionPage,
+    TimeoutsPage,
+    AdvancedPage,
+    FeatureRequestPage,
+    LrReceiverPage,
+    ContactUsPage,
+    AudioEffectPage,
+    ProductRegistrationPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    InAppBrowser,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    NativePageTransitions,
+    DatePicker
   ]
 })
-export class AppModule {}
+export class AppModule {
+
+
+}
